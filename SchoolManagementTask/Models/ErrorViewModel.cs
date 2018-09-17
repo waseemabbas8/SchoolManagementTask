4294d1ad2182c8d5e761d3c3a5f98c778312c567
Waseem <waseem@thetasolutions.co.uk>
2018-09-17 11:42:14 +0500

    Add project files.

---
+++ SchoolManagementTask/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolManagementTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
