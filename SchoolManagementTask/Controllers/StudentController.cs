e9d3f550b39ea1613ea2c68af728d083fdada413
Waseem <waseem@thetasolutions.co.uk>
2018-09-18 15:59:05 +0500

    Student Registration Flow

---
+++ SchoolManagementTask/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagementTask.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+    }
+}
--- SchoolManagementTask/Models/Student.cs
+++ SchoolManagementTask/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SchoolManagementTask.Models
 {
@@ -8,6 +9,7 @@ namespace SchoolManagementTask.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Class { get; set; }
+        [DataType(DataType.Date)]
         public DateTime? Dob { get; set; }
         public string Cnic { get; set; }
         public string Gender { get; set; }
