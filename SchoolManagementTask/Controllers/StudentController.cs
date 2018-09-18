035f22089e8455dad5b2556251a8cad5038c6e88
Waseem <waseem@thetasolutions.co.uk>
2018-09-18 18:15:42 +0500

    Student entry to Database

--- SchoolManagementTask/Controllers/StudentController.cs
+++ SchoolManagementTask/Controllers/StudentController.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SchoolManagementTask.Models;
 
 namespace SchoolManagementTask.Controllers
 {
     public class StudentController : Controller
     {
+        private dbLectureORM19AugContext ORM;
+        public StudentController(dbLectureORM19AugContext ORM)
+        {
+            this.ORM = ORM;
+        }
+
         public IActionResult AddStudent()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            try
+            {
+                ORM.Add(student);
+                ORM.SaveChanges();
+                ViewBag.Message = "Registration Done Succefully!";
+                ModelState.Clear();
+            }
+            catch
+            {
+                ViewBag.Message = "Error! Could not Save";
+            }
             return View();
         }
     }
